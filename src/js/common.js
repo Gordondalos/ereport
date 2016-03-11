824bf9056f90c66d507153b43ec78f70ad9b233e
Gordondalos <Gordondalos@gmail.com>
2016-03-11 17:14:45 +0600

    3 страница

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
+	//$.noConflict();
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -41,13 +35,65 @@ $(document).ready(function() {
 
 	};
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	// By default, swipe is enabled.
+	$('section').horizon();
+
+	// If you do not want to include another plugin, TouchSwipe, you can set it to false in the default options by passing in the option as false.
+	//$('section').horizon({swipe: false});
+
+	$(document).on('click', '.go-to-2', function () {
+		$(document).horizon('scrollTo', 'section-section2');
+	});
+
+
+
 	
 });
 
-$(window).load(function() {
+// джаваскрипт от панели
 
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
 
-});
+
+
+$(document).ready(function() {
+	var h = $(window).height();
+	$('.navigation-panel').css('height',""+h);
+
+	$('.bar').click(function(e){
+		e.preventDefault();
+		w = $('.navigation-panel').css('width');
+		if(w == '64px'){
+			$('.navigation-panel .fa-bars').css('transform','rotate(90deg)');
+			$('.navigation-panel .fa-bars').css('transition','.4s ease');
+			$('.navigation-panel').find('.links').animate({width:'85%'}, 500);
+			$('.navigation-panel').find('.links').fadeIn(500);
+			$('.navigation-panel').animate({width:'20rem'}, 400);
+			$('.navigation-panel').find('.superlative').fadeIn(400);
+		}else{
+
+			$('.navigation-panel .fa-bars').css('transform','rotate(0deg)');
+			$('.navigation-panel .fa-bars').css('transition','.4s ease');
+			$('.navigation-panel').find('.links').fadeOut(100);
+			$('.navigation-panel').find('.superlative').fadeOut(100);
+			$('.navigation-panel').animate({width:'4rem'}, 400);
+
+
+		}
+	});
+
+
+	$(window).on("orientationchange",function(){
+		if(window.orientation == 0) // Книжная
+		{
+			location.reload();
+		}
+		else // Альбомная
+		{
+			location.reload();
+		}
+	});
+
+
+
+}); // end of ready()
